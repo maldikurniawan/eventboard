a9f05c365ce15705acdedd3e6e85d4bfff494196
maldikurniawan <akurniawan2102@gmail.com>
2025-07-23 17:21:06 +0700

    add captcha

--- src/pages/attendance/AttendanceListPage.tsx
+++ src/pages/attendance/AttendanceListPage.tsx
@@ -3,29 +3,35 @@ import {
     Button,
     ButtonRipple,
     Limit,
+    Modal,
     Pagination,
     Tables,
     TextField,
     Tooltip
 } from "@/components";
 import { API_URL_event } from "@/constants";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { TbEye } from "react-icons/tb";
+import { TiArrowBackOutline } from "react-icons/ti";
 import { Link, useLocation } from "react-router-dom";
 import { useDebounceValue } from 'usehooks-ts';
-import { TiArrowBackOutline } from "react-icons/ti";
 
 interface AttendanceListInterface {
     nama: string;
     nohp: string;
     email: string;
     nama_perusahaan: string;
+    ip_address: string;
 }
 
 const AttendanceListPage = () => {
     const location = useLocation();
     const [searchTerm, setSearchTerm] = useState("");
     const [debounceSearch] = useDebounceValue(searchTerm, 500)
+    const [basicModal, setBasicModal] = useState<boolean>(false);
+    const [selectedData, setSelectedData] = useState<any>(null);
+    const [ipInfo, setIPInfo] = useState<any>(null);
     const { slug } = location.state;
     const { nama } = location.state;
 
@@ -53,6 +59,28 @@ const AttendanceListPage = () => {
     );
 
     useEffect(() => {
+        const fetchIPInfo = async () => {
+            if (!selectedData?.ip_address) {
+                setIPInfo(null);
+                return;
+            }
+            try {
+                const res = await axios.get(`https://ipwho.is/${selectedData.ip_address}`);
+                if (res.data?.success === false) {
+                    setIPInfo(null);
+                } else {
+                    setIPInfo(res.data);
+                }
+            } catch (err) {
+                console.error("Failed to fetch IP address", err);
+                setIPInfo(null);
+            }
+        };
+
+        fetchIPInfo();
+    }, [selectedData]);
+
+    useEffect(() => {
         setQueryParams((prev) => ({ ...prev, search: debounceSearch, offset: 0 }));
     }, [debounceSearch])
 
@@ -74,104 +102,270 @@ const AttendanceListPage = () => {
     const totalEntries = getEvent?.data?.count || 0;
     const currentPage = Math.floor(queryParams.offset / queryParams.limit) + 1;
 
-    // console.log(location?.state?.slug)
+    // console.log(ipInfo)
 
     return (
-        <div className="p-4 bg-[#333333] rounded-xl">
-            {/* Control Top */}
-            <div className="flex items-center mb-4 gap-2">
-                <Link to={"/attendance"}>
-                    <TiArrowBackOutline className="text-xl font-bold"/>
-                </Link>
-                <div className="text-xl font-bold text-center">
-                    {nama}
+        <>
+            <div className="p-4 bg-[#333333] rounded-xl">
+                {/* Control Top */}
+                <div className="flex items-center mb-4 gap-2">
+                    <Link to={"/attendance"}>
+                        <TiArrowBackOutline className="text-xl font-bold" />
+                    </Link>
+                    <div className="text-xl font-bold text-center">
+                        {nama}
+                    </div>
                 </div>
-            </div>
-            <div className="mb-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
-                <div className="w-full sm:w-60">
-                    <TextField
-                        type="text"
-                        placeholder="Search"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
+                <div className="mb-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
+                    <div className="w-full sm:w-60">
+                        <TextField
+                            type="text"
+                            placeholder="Search"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                    </div>
+                    <Button
+                        color="#BEBEBE"
+                        variant="outline"
+                        className="cursor-pointer"
+                        onClick={() => getEvent.refetch()}
+                    >
+                        Refresh
+                    </Button>
+                </div>
+
+                {/* Tables */}
+                <Tables>
+                    <Tables.Head>
+                        <Tables.Row>
+                            <Tables.Header>Nama</Tables.Header>
+                            <Tables.Header>No. HP</Tables.Header>
+                            <Tables.Header>Email</Tables.Header>
+                            <Tables.Header>Nama Perusahaan</Tables.Header>
+                            <Tables.Header>IP Address</Tables.Header>
+                            <Tables.Header center>Action</Tables.Header>
+                        </Tables.Row>
+                    </Tables.Head>
+                    <Tables.Body>
+                        {getEvent?.data?.results?.length > 0 ? (
+                            getEvent.data.results.map((item: AttendanceListInterface, itemIdx: number) => (
+                                <Tables.Row key={itemIdx}>
+                                    <Tables.Data>
+                                        <div className="capitalize">
+                                            {item.nama}
+                                        </div>
+                                    </Tables.Data>
+                                    <Tables.Data>{item.nohp}</Tables.Data>
+                                    <Tables.Data>{item.email}</Tables.Data>
+                                    <Tables.Data>{item.nama_perusahaan}</Tables.Data>
+                                    <Tables.Data>{item.ip_address ?? "-"}</Tables.Data>
+                                    <Tables.Data center>
+                                        <div className="flex items-center justify-center">
+                                            <Tooltip tooltip="Lihat">
+                                                <ButtonRipple
+                                                    stopPropagation
+                                                    onClick={() => {
+                                                        setBasicModal(true);
+                                                        setSelectedData(item);
+                                                    }}
+                                                    className="p-2 rounded-full transition-[background] hover:bg-white/10"
+                                                >
+                                                    <TbEye className="text-xl text-blue-500" />
+                                                </ButtonRipple>
+                                            </Tooltip>
+                                        </div>
+                                    </Tables.Data>
+                                </Tables.Row>
+                            ))
+                        ) : (
+                            <Tables.Row>
+                                <Tables.Data colspan={6} center>No Data</Tables.Data>
+                            </Tables.Row>
+                        )}
+
+                    </Tables.Body>
+                </Tables>
+
+                {/* Control Bottom */}
+                <div className="mt-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
+                    <div className="flex gap-2 items-baseline text-sm">
+                        <Limit
+                            limit={queryParams.limit}
+                            setLimit={handleSelect}
+                        />
+                        {totalEntries} entries
+                    </div>
+
+                    <Pagination
+                        totalCount={totalEntries}
+                        onPageChange={handlePageClick}
+                        currentPage={currentPage}
+                        pageSize={queryParams.limit}
                     />
                 </div>
-                <Button
-                    color="#BEBEBE"
-                    variant="outline"
-                    className="cursor-pointer"
-                    onClick={() => getEvent.refetch()}
-                >
-                    Refresh
-                </Button>
             </div>
-
-            {/* Tables */}
-            <Tables>
-                <Tables.Head>
-                    <Tables.Row>
-                        <Tables.Header>Nama</Tables.Header>
-                        <Tables.Header>No. HP</Tables.Header>
-                        <Tables.Header>Email</Tables.Header>
-                        <Tables.Header>Nama Perusahaan</Tables.Header>
-                        <Tables.Header>IP Address</Tables.Header>
-                        <Tables.Header center>Action</Tables.Header>
-                    </Tables.Row>
-                </Tables.Head>
-                <Tables.Body>
-                    {getEvent?.data?.results?.length > 0 ? (
-                        getEvent.data.results.map((item: AttendanceListInterface, itemIdx: number) => (
-                            <Tables.Row key={itemIdx}>
+            <Modal show={basicModal} setShow={setBasicModal} width="md" height="auto">
+                <div className="text-lg font-normal p-5">
+                    <div className="mb-3 font-bold border-b w-fit border-black">IP Information</div>
+                    <Tables>
+                        <Tables.Body>
+                            <Tables.Row>
+                                <Tables.Data>IP Address</Tables.Data>
                                 <Tables.Data>
-                                    <div className="capitalize">
-                                        {item.nama}
-                                    </div>
+                                    {
+                                        ipInfo?.ip ?? "N/A"
+                                    }
                                 </Tables.Data>
-                                <Tables.Data>{item.nohp}</Tables.Data>
-                                <Tables.Data>{item.email}</Tables.Data>
-                                <Tables.Data>{item.nama_perusahaan}</Tables.Data>
-                                <Tables.Data>-</Tables.Data>
-                                <Tables.Data center>
-                                    <div className="flex items-center justify-center">
-                                        <Tooltip tooltip="Lihat">
-                                            <ButtonRipple
-                                                stopPropagation
-                                                className="p-2 rounded-full transition-[background] hover:bg-white/10"
-                                            >
-                                                <TbEye className="text-xl text-blue-500" />
-                                            </ButtonRipple>
-                                        </Tooltip>
-                                    </div>
+                                <Tables.Data>
+                                    <p className="pl-1">Type</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.type ?? "N/A"
+                                    }
                                 </Tables.Data>
                             </Tables.Row>
-                        ))
-                    ) : (
-                        <Tables.Row>
-                            <Tables.Data colspan={6} center>No Data</Tables.Data>
-                        </Tables.Row>
-                    )}
+                            <Tables.Row>
+                                <Tables.Data>Continent</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.continent ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">Country</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.country ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Region</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.region ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">City</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.city ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Latitude</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.latitude ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">Longitude</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.longitude ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Postal</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.postal || "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">Calling Code</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.calling_code ? `+${ipInfo?.calling_code}` : "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Capital</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.capital ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">Borders</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.borders ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Flag</Tables.Data>
+                                <Tables.Data>
+                                    {ipInfo?.flag && ipInfo?.flag.img ? (
+                                        <img
+                                            src={ipInfo?.flag.img}
+                                            alt="flag"
+                                            className="w-6 h-auto"
+                                        />
+                                    ) : (
+                                        "N/A"
+                                    )}
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">ASN</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.connection.asn ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>ISP</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.connection.isp ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">ORG</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.connection.org ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                            <Tables.Row>
+                                <Tables.Data>Domain</Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.connection.domain ?? "N/A"
+                                    }
+                                </Tables.Data>
+                                <Tables.Data>
+                                    <p className="pl-1">Timezone</p>
+                                </Tables.Data>
+                                <Tables.Data>
+                                    {
+                                        ipInfo?.timezone.id ?? "N/A"
+                                    }
+                                </Tables.Data>
+                            </Tables.Row>
+                        </Tables.Body>
+                    </Tables>
 
-                </Tables.Body>
-            </Tables>
-
-            {/* Control Bottom */}
-            <div className="mt-4 flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-4">
-                <div className="flex gap-2 items-baseline text-sm">
-                    <Limit
-                        limit={queryParams.limit}
-                        setLimit={handleSelect}
-                    />
-                    {totalEntries} entries
                 </div>
-
-                <Pagination
-                    totalCount={totalEntries}
-                    onPageChange={handlePageClick}
-                    currentPage={currentPage}
-                    pageSize={queryParams.limit}
-                />
-            </div>
-        </div>
+            </Modal>
+        </>
     )
 }
 
--- src/pages/event/FormAttendance.tsx
+++ src/pages/event/FormAttendance.tsx
@@ -2,8 +2,10 @@ import { useGetData, usePostData } from "@/actions";
 import { Button, TextField } from "@/components";
 import { API_URL_event } from "@/constants";
 import { showToast } from "@/utils/showToast";
+import { Turnstile } from "@marsidev/react-turnstile";
+import axios from "axios";
 import { useFormik } from "formik";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TbLoader, TbLoader2 } from "react-icons/tb";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import * as Yup from "yup";
@@ -11,11 +13,26 @@ import * as Yup from "yup";
 const FormAttendance: React.FC = () => {
     const { slug } = useParams();
     const navigate = useNavigate();
+    const turnstileRef = useRef<any>(null);
     const [loading, setLoading] = useState<boolean>(false);
 
     const getEvent = useGetData(API_URL_event, ["event"], true);
     const dataEvent = getEvent.data?.results?.find((item: any) => item.slug === slug);
     const createEventAttendance = usePostData(`${API_URL_event}${dataEvent?.slug}/attendance/`, true);
+    const [ipAddress, setIpAddress] = useState<string>("");
+
+    useEffect(() => {
+        const fetchIP = async () => {
+            try {
+                const res = await axios.get("https://api.ipify.org/?format=json");
+                setIpAddress(res.data.ip);
+            } catch (err) {
+                console.error("Failed to fetch IP address", err);
+            }
+        };
+
+        fetchIP();
+    }, []);
 
     const formik = useFormik({
         initialValues: {
@@ -23,6 +40,7 @@ const FormAttendance: React.FC = () => {
             nohp: "",
             email: "",
             nama_perusahaan: "",
+            token: "",
         },
         validationSchema: Yup.object({
             nama: Yup.string().required("Nama wajib diisi"),
@@ -37,14 +55,20 @@ const FormAttendance: React.FC = () => {
                 .email("Format email tidak valid"),
 
             nama_perusahaan: Yup.string().required("Nama perusahaan wajib diisi"),
+            token: Yup.string().required("Wajib Verifikasi"),
         }),
         onSubmit: (values, { resetForm }) => {
             setLoading(true);
-            createEventAttendance.mutate(values as any, {
+            const payload = {
+                ...values,
+                ip_address: ipAddress,
+            };
+            createEventAttendance.mutate(payload as any, {
                 onSuccess: (res: any) => {
                     const data = res as { message: string };
                     resetForm();
                     setLoading(false);
+                    turnstileRef.current?.reset();
                     navigate("/result/" + dataEvent?.slug);
                     showToast(data.message, "success", 3000, true, true);
                 },
@@ -131,7 +155,21 @@ const FormAttendance: React.FC = () => {
                                 onBlur={formik.handleBlur}
                                 error={formik.touched.nama_perusahaan && formik.errors.nama_perusahaan}
                             />
-                            <div className="pt-4">
+                            <div>
+                                <Turnstile
+                                    ref={turnstileRef}
+                                    siteKey="0x4AAAAAABl2x7WFwOKjZG3J"
+                                    onSuccess={(e) => formik.setFieldValue("token", e)}
+                                />
+
+                                {formik.touched.token && formik.errors.token ? (
+                                    <div className="text-red-500 text-xs">
+                                        {formik.errors.token}
+                                    </div>
+                                ) : null}
+                            </div>
+
+                            <div className="pt-2">
                                 <Button
                                     type="submit"
                                     disabled={loading}
